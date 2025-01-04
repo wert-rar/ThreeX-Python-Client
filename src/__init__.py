4566744963d596eeb5eac27e6f346772761d13f9
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-04 18:50:30 +0300

    Add structure

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+from async_client import ThreeXClientAsync
+from client import ThreeXClient
+from Payload import Payload
+from DefaultPayload import DefaultPayload
+from errors import ClientError
+
+__version__ = '0.1.0'
+__author__ = 'Wert Rar'
+
+__all__ = ['ThreeXClient', 'ThreeXClientAsync', 'Payload', 'DefaultPayload', 'ClientError']
