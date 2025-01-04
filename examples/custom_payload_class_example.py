7de9f4771c3e477cf5c3970f1266a597f8db34a7
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-04 18:50:56 +0300

    Create custom_payload_class_example.py

---
+++ examples/custom_payload_class_example.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+
+from src.DefaultPayload import DefaultPayload
+
+class X3UIPayload(DefaultPayload):
+    def __init__(self, inbound_id, client_id, email, limitip, expiry_time, subid, custom_field1, custom_field2):
+        super().__init__(inbound_id, client_id, email, limitip, expiry_time,subid,
+                         additional_fields= {"custom_field1": custom_field1, "custom_field2": custom_field2}  )
+
+
+def test():
+    payload = X3UIPayload(12345, "aaaa-aaaa-aaaa-aaaa", "john.doe@example.com", 3,17000000000, "1234567890", "Custom Field 1", "Custom Field 2")
+    pprint(payload.format())
+
+
+if __name__ == "__main__":
+    test()
