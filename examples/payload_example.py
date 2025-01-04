7b8e939d22dc73b3cb788eb6d781420d70b0f708
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-04 18:50:51 +0300

    Create payload_example.py

---
+++ examples/payload_example.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+
+from src.DefaultPayload import DefaultPayload
+
+
+def get_payload(inbound_id:int, client_id, email, limitip, expiryTime, subid,
+             flow="xtls-rprx-vision", totalGB=0, enable=True, reset=0, additional_fields: dict = {}):
+    """
+      Generate a payload dictionary for client configuration.
+
+      This function creates a structured payload containing client information
+      for use in API requests or configuration settings.
+
+      Requaried Parameters:
+      inbound_id (str): The ID of the inbound connection.
+      client_id (str): The unique identifier for the client.
+      email (str): The email address associated with the client.
+      limitip (int): The maximum number of IP addresses allowed for this client.
+      expiryTime (int): The expiration time for the client's access.
+
+      subid (str): The subscription ID for the client.
+
+      Default Parameters:
+      flow (str): The TLS/SSL protocol to use for the client's connection.
+      totalGB (int): The total storage capacity for the client (in GB).
+      enable (bool): Whether the client should be enabled or disabled.
+      reset (int): The number of days before the client's access expires.
+
+      Additional Parameters:
+      additional_fields (dict): Additional fields to be included in the payload.
+
+      Returns:
+      dict: A dictionary containing the structured payload with client information.
+            The 'settings' key contains a JSON string with detailed client configuration.
+    """
+
+    payload = DefaultPayload(inbound_id=inbound_id,client_id=client_id, email=email, limitip=limitip, expiryTime=expiryTime, subid=subid,additional_fields=additional_fields)
+    return payload.format()
+
+
+if __name__ == "__main__":
+
+    # Example usage without custom fields
+    payload1 = get_payload(12345, 'aaaa-aaaa-aaaa-aaaa', 'aaaaaaa', 3, 1700000000, 'dbakdahadjwhwadba')
+    print('Payload 1 (without custom fields)')
+    pprint(payload1)
+    print()
+    # Example usage with custom fields
+    payload2 = get_payload(12345, 'aaaa-aaaa-aaaa-aaaa', 'aaaaaaa', 3, 1700000000, 'dbakdahadjwhwadba', additional_fields={'tgId': 1234567890})
+    print('Payload 2 (with custom fields)')
+    pprint(payload2)
+
+
