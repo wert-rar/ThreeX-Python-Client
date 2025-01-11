8dc1c761d4e2dec04749ea98339790fb7719863b
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-11 19:19:17 +0300

    Update __init__.py

--- treex_ui_client/treex_ui_client/__init__.py
+++ treex_ui_client/treex_ui_client/__init__.py
@@ -1,7 +1,11 @@
 from treex_ui_client.treex_ui_client.AsyncClient import AsyncClient3XUI
 from treex_ui_client.treex_ui_client.Client import Client3XUI
 from treex_ui_client.treex_ui_client.payload import Payload
-from treex_ui_client.treex_ui_client.default_payload import DefaultPayload
+from treex_ui_client.treex_ui_client.ClientPayload import CLientPayload
+from treex_ui_client.treex_ui_client.InboundPayload import InboundPayload
 from treex_ui_client.treex_ui_client.errors import ClientError
 
-__all__ = ['Client3XUI', 'AsyncClient3XUI', 'Payload', 'DefaultPayload', 'ClientError']
+
+# Add new methods for async client
+
+__all__ = ['Client3XUI', 'AsyncClient3XUI', 'Payload', 'CLientPayload', 'ClientError','InboundPayload']
