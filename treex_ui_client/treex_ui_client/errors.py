be9fbc0d2dd7c2c3b6571c2871784482edbb5d3d
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-16 21:09:33 +0300

    Add doc strings to ClientError

--- treex_ui_client/treex_ui_client/errors.py
+++ treex_ui_client/treex_ui_client/errors.py
@@ -1,7 +1,28 @@
 class ClientError(Exception):
-    def __int__(self, text,status):
+    """
+    This class extends the built-in Exception class to provide specific
+    error handling for client-related issues.
+    """
+
+    def __init__(self, text, status):
+        """
+        Initialize a new ClientError instance.
+
+        Args:
+            text (str): A description of the error.
+            status (int): The status code associated with the error.
+
+        Returns:
+            None
+        """
         self.txt = text
         self.status = status
 
     def __repr__(self):
+        """
+        Return a string representation of the ClientError.
+
+        Returns:
+            str: A formatted string containing the error message and status code.
+        """
         return f'ClientError:  {self.txt} \nstatus: [{self.status}]'
