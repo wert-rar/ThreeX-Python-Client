58cdc079af8cc69b3467b22a08390ef2ac2b3b62
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-01-16 21:48:11 +0300

    fix __init__ problem

--- treex_ui_client/___Init__.py
+++
@@ -1,3 +0,0 @@
-from treex_ui_client.treex_ui_client import Client3XUI, Payload, CLientPayload,AsyncClient3XUI, ClientError
-
-__all__ = ['Client3XUI', 'Payload', 'CLientPayload', 'AsyncClient3XUI', 'ClientError']
